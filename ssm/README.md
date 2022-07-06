5c82fffbc601a61e9a1ee95116da16fcc9c47740
Sakamoto0525 <525masaya@gmail.com>
2022-07-07 07:51:14 +0900

    コメント追加

--- ssm/README.md
+++ ssm/README.md
@@ -3,3 +3,5 @@
 パラメータはコンソールから直接作成する
 
 また、~/.aws/credentialsにAWSのアカウント設定がされている前提
+
+[AWS SDK for Go API Reference](https://docs.aws.amazon.com/sdk-for-go/api/service/ssm/)
--- ssm/main.go
+++ ssm/main.go
@@ -9,13 +9,20 @@ import (
 )
 
 func main() {
+	// AWSサービスと接続するための共通設定。利用するリージョンやAWSでの認証方法を設定可能。
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String("ap-northeast-1"),
+		// Profile: "default",
 	}))
+
+	// ssm (AWS System Manager)を利用するための設定
 	svc := ssm.New(sess)
 
+	// ストアパラメータからNamesに指定したキーと一致した値を全て取得する
 	res, err := svc.GetParameters(&ssm.GetParametersInput{
-		Names:          []*string{aws.String("/name")},
+		Names: []*string{aws.String("/name")},
+
+		// trueの場合はパラメータが暗号化している場合に複合化して取得する
 		WithDecryption: aws.Bool(true),
 	})
 	if err != nil {
